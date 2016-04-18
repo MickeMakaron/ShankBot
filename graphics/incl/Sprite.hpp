416673d1c939588f7f7cbf32d1a9b956e7c96011
MickeMakaron <mikael.hernvall@gmail.com>
2016-04-18 18:58:26 +0200

    Simple sprite struct

---
+++ graphics/incl/Sprite.hpp
@@ -0,0 +1,18 @@
+#ifndef GRAPHICS_LAYER_SPRITE
+#define GRAPHICS_LAYER_SPRITE
+
+///////////////////////////////////
+// STD C++
+#include <string>
+///////////////////////////////////
+
+namespace GraphicsLayer
+{
+    struct Sprite
+    {
+        std::string name;
+    };
+}
+
+
+#endif // GRAPHICS_LAYER_SPRITE
