87fdc2ae817103ed5c546aa92c6029d3773336db
Christian Hernvall <christian.hernvall@gmail.com>
2016-04-20 12:42:23 +0200

    Added ugly fix for jalapeños. Also faster

--- crawler/gief-them-gifs-from-generic-categories.py
+++ crawler/gief-them-gifs-from-generic-categories.py
@@ -23,31 +23,35 @@ def download_giefs(soup):
     pages = soup.find('div', id='mw-pages')
     table = pages.find('table')
     for page in table.findAll('a'):
-        if "Template:" in page['href']:
-            print "Disallowed by robots!" + page['href']
+        href = page['href']
+        gif_name = href.replace("/wiki/", "").replace("_"," ") + ".gif"
+        if "Template:" in href:
+            print "Disallowed by robots!" + href
             continue
-        specific_page = mech.open(base_url + page['href'])
+        elif os.path.isfile(os.path.join(save_dir, gif_name)):
+            print gif_name + " is already downloaded"
+            continue
+        elif "%C3%B1" in href: # Fix unicode ugly hack
+            print "PYTHON NO HANDLE UNICODE FOR YOU. YOU HAVE TO DOWNLOAD THIS YOURSELF " + base_url + href
+            continue
+        specific_page = mech.open(base_url + href)
         specific_html = specific_page.read()
         specific_soup = BeautifulSoup(specific_html)
         image_box = specific_soup.find('div', id='twbox-image')
 
         if not image_box:
-            print "Found a non-standard page (" + page['href'] + "). It does not contain an image."
+            print "Found a non-standard page (" + href + "). It does not contain an image."
         elif not image_box.find('a'):
-            print "Did not find any image link. " + page['href']
+            print "Did not find any image link. " + href
         elif "Special:Upload" in image_box.a['href']:
             print "No image available for " + image_box.a.string.replace("File:","").replace(".gif","")
         else:
             gif_url = image_box.a['href']
-            gif_name = image_box.a.img['data-image-name']
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
             save_as = os.path.join(save_dir, gif_name)
-            if os.path.isfile(save_as):
-                print gif_name + " is already downloaded"
-            else:
-                urllib.urlretrieve(gif_url, save_as)
-                print "Downloaded %s to %s" % (gif_name, save_dir)
+            urllib.urlretrieve(gif_url, save_as)
+            print "Downloaded %s to %s" % (gif_name, save_dir)
 
 for top_level_category in top_level_categories:
     print "In category " + top_level_category
