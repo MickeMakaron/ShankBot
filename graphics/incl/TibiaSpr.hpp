aa24ad0630b0e61d70cec7e307554b83cffaa4da
MickeMakaron <mikael.hernvall@gmail.com>
2016-05-07 00:25:23 +0200

    Tibia.spr reader

---
+++ graphics/incl/TibiaSpr.hpp
@@ -0,0 +1,35 @@
+#ifndef GRAPHICS_LAYER_TIBIA_SPR
+#define GRAPHICS_LAYER_TIBIA_SPR
+
+///////////////////////////////////
+// STD C++
+#include <vector>
+#include <string>
+#include <istream>
+///////////////////////////////////
+
+namespace GraphicsLayer
+{
+    class TibiaSpr
+    {
+        public:
+            TibiaSpr(std::string sprFilePath);
+
+            const std::vector<std::vector<unsigned char>>& getSprites() const;
+            const std::vector<size_t>& getSpriteIds() const;
+
+        private:
+            void readSprites(std::string sprFilePath);
+            std::vector<unsigned char> readOpaqueSpritePixels(std::istream& spr) const;
+
+
+        private:
+            unsigned int mVersion;
+            unsigned int mNumSprites;
+            std::vector<std::vector<unsigned char>> mSprites;
+            std::vector<size_t> mIds;
+    };
+}
+
+
+#endif // GRAPHICS_LAYER_TIBIA_SPR
