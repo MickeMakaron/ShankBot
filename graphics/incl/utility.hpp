6616c10d0e8d98da9a83ccc2088ed3dd226e2b3a
MickeMakaron <mikael.hernvall@gmail.com>
2016-05-07 00:24:43 +0200

    Simple function for reading data from stream.

--- graphics/incl/utility.hpp
+++ graphics/incl/utility.hpp
@@ -6,6 +6,7 @@
 // STD C++
 #include <string>
 #include <list>
+#include <istream>
 #include "PngImage.hpp"
 ///////////////////////////////////
 
@@ -16,6 +17,12 @@ namespace GraphicsLayer
     unsigned char* rgbaToRgb(const unsigned char* rgba, size_t width, size_t height);
     void testLoadImages(std::string directory, std::list<PngImage>& images);
     void readPpm(std::string filePath, unsigned char** data, size_t* width, size_t* height);
+
+    template<typename T>
+    void readStream(T& out, std::istream& stream)
+    {
+        stream.read((char*)&out, sizeof(T));
+    }
 }
 
 #endif // GRAPHICS_LAYER_UTILITY
