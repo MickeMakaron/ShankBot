357440e37ba1e76191a9a7572e361771916954d8
MickeMakaron <mikael.hernvall@gmail.com>
2016-05-07 15:12:22 +0200

    Cleaned up and updated towards changes.

--- graphics/src/main.cpp
+++ graphics/src/main.cpp
@@ -1,28 +1,7 @@
-#include "TibiaClient.hpp"
-#include "ImageTree.hpp"
-
-#include <iostream>
-
-using namespace GraphicsLayer;
+#include "ShankBot.hpp"
 
 int main(int argc, char *args[])
 {
-    std::cout << "Constructing tree..." << std::endl;
-    ImageTree tree("tibia/tree.bin");
-
-    TibiaClient client("./tibia", tree);
-    while(true)
-    {
-        client.update();
-//
-//        for(const std::list<size_t> ids : client.getDrawnSprites())
-//        {
-//            for(size_t id : ids)
-//                std::cout << id << " ";
-//            std::cout << std::endl;
-//        }
-        std::list<std::list<size_t>> drawnSprites = client.getDrawnSprites();
-        if(!drawnSprites.empty())
-            std::cout << "Found " << drawnSprites.size() << std::endl;
-    }
+    GraphicsLayer::ShankBot sb("tibia", "version-control");
+    sb.run();
 }
