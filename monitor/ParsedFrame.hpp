8c45375df01f3deaca92f44b0faa741a00466852
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-03-22 15:49:31 +0100

    Add ParsedFrame struct

---
+++ monitor/ParsedFrame.hpp
@@ -0,0 +1,55 @@
+// {SHANK_BOT_LICENSE_BEGIN}
+/****************************************************************
+****************************************************************
+*
+* ShankBot - Automation software for the MMORPG Tibia.
+* Copyright (C) 2016-2017 Mikael Hernvall
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+* Contact:
+*       mikael.hernvall@gmail.com
+*
+****************************************************************
+****************************************************************/
+// {SHANK_BOT_LICENSE_END}
+#ifndef GRAPHICS_LAYER_PARSED_FRAME_HPP
+#define GRAPHICS_LAYER_PARSED_FRAME_HPP
+
+///////////////////////////////////
+// Internal ShankBot headers
+#include "monitor/Gui.hpp"
+#include "monitor/RawImage.hpp"
+namespace GraphicsLayer
+{
+    class TibiaContext;
+}
+///////////////////////////////////
+
+///////////////////////////////////
+// STD C++
+#include <memory>
+///////////////////////////////////
+
+namespace GraphicsLayer
+{
+    struct ParsedFrame
+    {
+        const TibiaContext* context;
+        std::shared_ptr<Gui::Data> gui;
+        std::shared_ptr<RawImage> screenPixels;
+    };
+}
+
+#endif // GRAPHICS_LAYER_PARSED_FRAME_HPP
