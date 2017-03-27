48aa54b61638ec72d8f52d17fbdeb8a07122e1d3
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-03-27 14:29:28 +0200

    Add FrameAssembler class

---
+++ monitor/FrameAssembler.hpp
@@ -0,0 +1,60 @@
+// {SHANK_BOT_LICENSE_BEGIN}
+/****************************************************************
+****************************************************************
+*
+* ShankBot - Automation software for the MMORPG Tibia.
+* Copyright (C) 2016-2017 Mikael Hernvall
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+* Contact:
+*       mikael.hernvall@gmail.com
+*
+****************************************************************
+****************************************************************/
+// {SHANK_BOT_LICENSE_END}
+#ifndef GRAPHICS_LAYER_FRAME_ASSEMBLER_HPP
+#define GRAPHICS_LAYER_FRAME_ASSEMBLER_HPP
+
+///////////////////////////////////
+// Internal ShankBot headers
+#include "monitor/Gui.hpp"
+#include "monitor/Scene.hpp"
+namespace GraphicsLayer
+{
+    class TibiaContext;
+    struct Frame;
+}
+///////////////////////////////////
+
+///////////////////////////////////
+// STD C++
+///////////////////////////////////
+
+namespace GraphicsLayer
+{
+    class FrameAssembler
+    {
+        public:
+            FrameAssembler(const TibiaContext& context);
+
+            void update(const Frame& frame);
+
+        private:
+            Gui mGui;
+            Scene mScene;
+    };
+}
+
+#endif // GRAPHICS_LAYER_FRAME_ASSEMBLER_HPP
